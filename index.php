3cd91086d089cfb307c4326b85dc1f789567af2a
Minhazul Abedin Munna <64527538+smmunna@users.noreply.github.com>
2022-05-17 20:34:12 +0600

    commited 1

---
+++ index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/bootstrap.min.css">
+    <link rel="stylesheet" href="style/style.css">
+    <title>Home</title>
+</head>
+<body>
+
+<div class="container">
+        <section class="cov1">
+            <div class="row">
+                <div class="col-lg-6">
+                        <div class="content1">
+                            <h2 class="heading1"><span class="heading2">Skip</span> The Diet,<br>Just Eat Healthy <br>With Food Network</h2>
+                            <p class="heading3">Imagine you don't need a diet because we provided healthy and delicious food for you!</p>
+                            <button class="btn1"><span class="heading4">Order Food</span></button>
+                        </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="covimage">
+                        <img src="images/stone.png" alt="" srcset="">
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div> 
+
+    <!-- Making the 1st Section -->
+    <section class="sec1">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="covimage2">
+                    <img src="images/mynew.png" alt="" srcset="">
+                </div>
+            </div>
+            <div class="col-lg-6">
+                    <div class="heading5">
+                        <p class="heading6">About Me</p>
+                        <p class="heading7">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi minima saepe, accusamus veritatis dolor iure! Laboriosam porro commodi velit? Aspernatur cum dolor consequatur et! Quae corporis id, dignissimos numquam atque, consectetur, quos suscipit molestias omnis earum facere minus iste fugit.</p>
+                        <button class="btn2"><span class="heading4">Contact Me</span></button>
+                    </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="mycontents">
+                    <p class="heading8">My Recepies</p>
+                    <p class="heading9">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quam animi consequuntur dicta quas delectus, quaerat aperiam molestias a nesciunt odio totam voluptatibus quisquam cumque incidunt sit modi quae? Deleniti.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="sec3">
+         
+        <div class="row">
+                <div class="col-lg-4">
+                        <div class="card" >
+                            <img src="images/project1.jpg" class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Recepie Title</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    </div>
+                        </div>
+                </div>
+                    <div class="col-lg-4">
+                                <div class="card">
+                                        <img src="images/cov1.jpg" class="card-img-top" alt="...">
+                                                <div class="card-body">
+                                                    <h5 class="card-title">Recepie Title</h5>
+                                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                                </div>
+                                    
+                                </div>
+                    </div>
+                        <div class="col-lg-4">
+                                    <div class="card">
+                                        <img src="images/cov2.png" class="card-img-top" alt="...">
+                                                <div class="card-body">
+                                                    <h5 class="card-title">Recepie Title</h5>
+                                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                                </div>
+                                    </div>
+                        </div>
+         
+            </div> 
+            
+
+            </div>
+
+
+            <!-- New section making for subscribers -->
+            <div class="sec4">
+                    <div class="sec5">
+                        <div class="row">
+                            <div class="col-lg-4 mycol">
+                                <p class="heading10">700k</p>
+                                <p class="heading11">Youtube Subscribers</p>
+                            </div>
+                            <div class="col-lg-4 mycol">
+                                <p class="heading10">2.4m</p>
+                                <p class="heading11">Instragram Subscribers</p>
+                            </div>
+                            <div class="col-lg-4 mycol">
+                                <p class="heading10">100k</p>
+                                <p class="heading11">Dribble Subscribers</p>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+
+            <!-- New Section for footer -->
+            <div class="sec6">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <p class="heading12"><span class="heading14">FOOD</span> <span class="heading15">NETWORK</span></p>
+                        <p class="heading13">Eat healthy to live healthy. Live healthy to live happy.</p>
+                    </div>
+                </div>
+            </div>
+
+    </section>
+
+</div>
+<script src="style/bootstrap.min.js"></script>
+</body>
+</html>
